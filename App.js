564bf22b460d5dd5ba2f2dab0086d6ab7a45b8b1
Lahwin <lahwin.l123@gmail.com>
2021-10-19 10:42:14 +0700

    first commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! นายหล้าวิน ลุงกอหลิ่ง 63309010068</Text>
       <StatusBar style="auto" />
     </View>
   );
